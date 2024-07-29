c44695425edeaab5f2875c23fcb94350c2a9d768
Brad Richardson <brichardson2468@gmail.com>
2024-07-29 13:45:36 -0400

    added insert route

--- app.py
+++ app.py
@@ -29,3 +29,19 @@ def creating():
     conn.commit()
     conn.close()
     return "Basketball table successfully created!"
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgresql://brad_lab_10_postgresql_user:MBvCTh8jJemI9zbgrZ3uu46pXCfrnFzU@dpg-cqjsrh8gph6c739ds0i0-a/brad_lab_10_postgresql")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+        Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball table successfully populated!"
